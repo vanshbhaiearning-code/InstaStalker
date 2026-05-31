bcce4e0d0d06b21e184f651739dcc296a3e6064f
vanshbhaiearning-code <vanshbhaiearning@gmail.com>
2026-05-31 15:42:44 +0530

    Create forceJoin.js

---
+++ keyboards/forceJoin.js
@@ -0,0 +1,18 @@
+module.exports = {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        {
+          text: "📢 Join Channel",
+          url: "https://t.me/latestmodsapks"
+        }
+      ],
+      [
+        {
+          text: "✅ Verify",
+          callback_data: "verify"
+        }
+      ]
+    ]
+  }
+};
